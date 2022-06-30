c29250004565ac7a281f2ba8d4c07c18276e4765
himokkk <himok.pk@gmail.com>
2022-06-30 17:56:16 +0200

    Login front

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login  from "./login/login";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Login/>      
     </View>
   );
 }
-
+// #32465f - gray
+// #5af0b9 - blue
+// #798ca6 - gray text
+const gray = '#32465f';
+const blue = '#5af0b9';
+const grayText = '#798ca6';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
