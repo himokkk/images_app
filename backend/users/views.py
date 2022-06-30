3ec661391ef2a09a8edcb06a0caf4389b77a8624
himokkk <himok.pk@gmail.com>
2022-06-30 18:18:09 +0200

    Login frontend+backend

--- backend/users/views.py
+++ backend/users/views.py
@@ -7,15 +7,16 @@ from rest_framework.settings import api_settings
 
 
 class LoginView(ObtainAuthToken):    
+    authentication_classes = []
+    permission_classes = []
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
     def post(self, request, *args, **kwargs):
-        print("XD")
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})        
         serializer.is_valid(raise_exception=True)        
         user = serializer.validated_data['user']  
-        print(user) 
+        print("Logged in as:",user) 
         login(request, user) 
              
         token, created = Token.objects.get_or_create(user=user)
